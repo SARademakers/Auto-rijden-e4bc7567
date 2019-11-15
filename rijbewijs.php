d263a7c764cafac87b81efa8b765f7c75c17a2fd
SARademakers <2049137@talnet.nl>
2019-11-15 09:26:51 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,10 @@
+<?php
+echo ("Hoe oud ben je?" . PHP_EOL);
+$line = readline();
+$a =  16.5;
+if ($line < $a) {
+	echo ("Helaas, je mag nog niet beginnen met rijlessen");
+}
+if ($line > $a) {
+	echo ("Je mag beginnen met rijlessen!");
+}
